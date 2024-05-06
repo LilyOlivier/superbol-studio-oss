a1308f941f1ca555e2055d27a854288b312e0342
LilyOlivier <101394789+LilyOlivier@users.noreply.github.com>
2024-05-06 08:48:33 +0000

    Deploying to gh-pages from  @ fc4fc2503b52e7a6cad1f788f8ce0da767c6ff1a 🚀

---
+++ sphinx/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'master (fc4fc25) (2024/05/06 08:48)',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
